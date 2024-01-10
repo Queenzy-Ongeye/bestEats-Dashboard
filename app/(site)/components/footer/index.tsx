4caf0d1e11a112969bea9d6ad05164e851bfd63b
Queenzy-Ongeye <quienzy@food4education.org>
2024-01-10 15:26:09 +0300

    fixed date format in footer component

--- app/(site)/components/footer/index.tsx
+++ app/(site)/components/footer/index.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 const Footer = () => {
+  const date = new Date().getFullYear() + ",";
   return (
     <footer className="pt-4">
       <div className="w-full px-6 mx-auto">
         <div className="flex flex-wrap items-center -mx-3 lg:justify-between">
           <div className="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
             <div className="text-sm leading-normal text-center text-slate-500 lg:text-left">
-              ©<script>document.write(new Date().getFullYear() + ",");</script>
+              ©<span>{`${date}`}</span>
               made with <i className="fa fa-heart"></i> by
               <a
-                href="https://www.creative-tim.com"
+                href="#"
                 className="font-semibold text-slate-700"
                 target="_blank"
               >
-                Salama e-shop
+                Best Eats
               </a>
               for a better living.
             </div>
@@ -27,7 +28,7 @@ const Footer = () => {
                   className="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500"
                   target="_blank"
                 >
-                  Salama e-shop
+                  Best Eats
                 </a>
               </li>
               <li className="nav-item">
