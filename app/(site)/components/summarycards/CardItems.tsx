46451f0f0ba42aa4407ee8a94c53b988781ada5e
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-04-23 10:17:52 +0300

    added card items and resized icons

--- app/(site)/components/summarycards/CardItems.tsx
+++ app/(site)/components/summarycards/CardItems.tsx
@@ -3,8 +3,8 @@ import { IoAnalyticsOutline } from "react-icons/io5";
 import { LuCircleDollarSign } from "react-icons/lu";
 
 export const CardItems = [
-    {title: "Value", figure: "$30,000", percentage: "+4.4%", icon : <LuCircleDollarSign/>},
-    {title: "Users", figure: "50,021", percentage: "+2.6%", icon : <HiOutlineUsers/>},
-    {title: "Orders", figure: "45,021", percentage: "+3.1%", icon : <IoAnalyticsOutline/>},
-    {title: "Tickets", figure: "20,516", percentage: "+3.1%", icon : <HiOutlineTicket/>},
+    {title: "Value", figure: "$30,000", percentage: "+4.4%", icon : <LuCircleDollarSign size={32} />},
+    {title: "Users", figure: "50,021", percentage: "+2.6%", icon : <HiOutlineUsers size={32}/>},
+    {title: "Orders", figure: "45,021", percentage: "+3.1%", icon : <IoAnalyticsOutline size={32} />},
+    {title: "Tickets", figure: "20,516", percentage: "+3.1%", icon : <HiOutlineTicket size={32} />},
 ]
