f6366df1407a712eb9d213b27dc3453a725439d9
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-04-26 11:24:55 +0300

    added invoice columns

--- app/(site)/components/tables/tableColumns.tsx
+++ app/(site)/components/tables/tableColumns.tsx
@@ -57,55 +57,94 @@ export const dashBoardData = [
   },
 ];
 
-export const ordersColumns:ColumnType[] = [
+export const ordersColumns: ColumnType[] = [
   {
     Header: "Order No.",
     accessor: "id",
-    sortType:"basic"
+    sortType: "basic",
   },
   {
     Header: "Customer Name",
     accessor: "customer_name",
-    sortType:"basic"
+    sortType: "basic",
   },
   {
     Header: "Dispatch Status",
     accessor: "dispatch_status",
     Cell: ({ value }: any) => (
-      <span className={`${value ? 'bg-green-500 text-white p-2 rounded-md' : 'bg-red-500 text-white p-2 rounded-md'}`}>
+      <span
+        className={`${
+          value
+            ? "bg-green-500 text-white p-2 rounded-md"
+            : "bg-red-500 text-white p-2 rounded-md"
+        }`}
+      >
         {value ? "Ready for Pickup" : "Not Dispatched"}
       </span>
     ),
-    sortType: "basic"
+    sortType: "basic",
   },
   {
     Header: "Item price",
     accessor: "price",
-    sortType: "basic"
+    sortType: "basic",
   },
   {
     Header: "Item Quantity",
     accessor: "quantity",
-    sortType: "basic"
+    sortType: "basic",
   },
   {
     Header: "Action",
     accessor: "",
     sortType: "basic",
     Cell: (row: any) => (
-      <span className="" >
+      <span className="">
         <ReusableButtonLink
-        href={"/orders/" + encodeURIComponent(row?.original?.id)}
+          href={"/orders/" + encodeURIComponent(row?.original?.id)}
         >
           View
         </ReusableButtonLink>
       </span>
-    ) 
+    ),
   },
 ];
 
-export const socialTrafficColumns = [
+export const InvoiceColumns = [
   {
-
-  }
-]
+    Header: "Invoice Number",
+    accessor: "invoice_number",
+    sortBy: "basic",
+  },
+  {
+    Header: "Account Name",
+    accessor: "account_name",
+    sortBy: "basic",
+  },
+  {
+    Header: "Amount",
+    accessor: "amount",
+    sortBy: "basic",
+  },
+  {
+    Header: "Date",
+    accessor: "invoice_date",
+    sortBy: "basic",
+  },
+  {
+    Header: "Status",
+    accessor: "status",
+    sortBy: "basic",
+    Cell: ({ value, isExpired }: any) => (
+      <span
+        className={`${
+          value
+            ? "inline-block px-2 py-px ml-2 text-md text-green-500 bg-green-100 rounded-md"
+            : "inline-block px-2 py-px ml-2 text-md text-red-400 bg-green-100 rounded-md"
+        }`}
+      >
+        {value ? "Paid" : isExpired ? "Expired" : "Overdue"}
+      </span>
+    ),
+  },
+];
